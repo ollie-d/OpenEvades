8679ce59592ca60c5b31dc73ca4605ed819c803f
ollie-d <ollie.damico@gmail.com>
2020-10-12 15:35:52 -0700

    Added logic to determine if enemy has collided with player or ally in order to handle death and revive mechanics. Also added the ability to revive the player by pressing the R key in order to debug more quickly. Currently hit function in Ball class calls kill() and revive(), revive is what is trigered by R only when player is dead.

--- Evades/Main.gd
+++ Evades/Main.gd
@@ -1,5 +1,7 @@
 extends Node
 
+export var Debug = true
+
 func _ready():
 	randomize()
 	
@@ -9,19 +11,20 @@ func _ready():
 	
 	# Add collision exceptions for player+safezones and enemies+enemies
 	# Also add exception for player+enemies (area handles death)
-	$Ball.add_collision_exception_with($LeftSafeZone)
-	$Ball.add_collision_exception_with($RightSafeZone)
+	$Player.add_collision_exception_with($LeftSafeZone)
+	$Player.add_collision_exception_with($RightSafeZone)
 	var time_before = OS.get_ticks_msec()
 	var children = $EnemyContainer.get_children()
 	for child in children:
-		child.add_collision_exception_with($Ball)
+		child.add_collision_exception_with($Player)
 		for child_ in children:
 			if child != child_:
 				child.add_collision_exception_with(child_)
 	var total_time = OS.get_ticks_msec() - time_before
 	print("Time taken: " + str(total_time))
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(_delta):
+	if Debug:
+		if !$Player.alive:
+			if Input.is_key_pressed(KEY_R):
+				$Player.revive()
